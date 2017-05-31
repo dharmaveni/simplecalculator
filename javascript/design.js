a11162618820ecc431a04eed82cff44f3ad55ca6
dharmaveni <dharmaveni1965@gmail.com>
2017-05-31 14:22:48 +0530

    creating simple calculator

---
+++ javascript/design.js
@@ -0,0 +1,16 @@
+var box=document.getElementByIDd('display');
+
+function addtoscreen(x);
+    {
+        box.value +=x;
+		if(x=='Del')
+		{
+		box.value='';
+	}
+}
+	function answer()
+	{
+		box.value=x;
+		x=eval(x);
+		box.value=x;
+	}
